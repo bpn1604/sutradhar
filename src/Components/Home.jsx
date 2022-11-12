7dd574592d83417cbc7a9ca0a730b1550511259a
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2022-11-12 11:13:59 +0530

    could not complete

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,32 +1,69 @@
-import React, { useState } from 'react'
+import React, { useRef, useState ,useEffect} from "react";
 //const data = [{id:1}, {id:2},{id:3}]
 export const Home = () => {
-const [data,setData] = useState([{id:1}, {id:2},{id:3},{id:"4"},{id:"5"},{id:6}, {id:7},{id:8},{id:"9"},{id:"10"}])
-const handleClick=(id)=>{
-  console.log(id)
+  const [data, setData] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+  ]);
+  //const [rightData, setRightData] = useState([]);
+  const [rightData,setRightData] = useState();
+  const handleClick = (el) => {
+    //console.log(id)
+
+    setRightData(el)
+    console.log(rightData);
+  };
   
-}
+  
   return (
     <>
-    <div >
+      <div>
         <input type="checkbox" />
         <label>COPY</label>
         <input type="checkbox" />
         <label>MOVE</label>
-
-    </div>
-    <div style={{height:"300px",marginLeft:"60px", border:"0.5px solid",width:"20%" , overflow:"scroll"}}>
-    {data.map((el)=>(
-      <div key={el.id} onClick={()=>handleClick(el.id)}>
-        <div style={{border:".3px solid",height:"120px solid" , margin:"auto", width:"80%" ,marginBottom:"15px" }}>bipin</div>
       </div>
-    ))}
-    </div>
-    
-    <div>
-        <div></div>
-        <div></div>
-    </div>
+      <div style={{display:"flex" ,margin:"10px"}}>
+        <div
+          style={{
+            height: "300px",
+            marginLeft: "60px",
+            border: "0.5px solid",
+            width: "20%",
+            overflow: "scroll",
+          }}
+        >
+          {data.map((el) => (
+            <div key={el.id} onClick={() => handleClick(el)}>
+              <div
+                style={{
+                  border: ".3px solid",
+                  height: "120px solid",
+                  margin: "auto",
+                  width: "80%",
+                  marginBottom: "15px",
+                }}
+              >
+                bipin
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+         
+        
+        </div>
+        
+      </div>
+     <button></button>
     </>
-  )
-}
+  );
+};
